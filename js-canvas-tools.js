4d7ac6e0e497d4c05f06703179a9e855e8936a7a
GrinsteadDev <devin.grinstead@grinsteaddev.com>
2022-02-19 08:23:29 -0500

    update js-canvas-tools.js

---
+++ js-canvas-tools.js
@@ -0,0 +1,15 @@
+function importModule() {
+
+}
+
+Object.defineProperty(importModule, 'responseType', {
+    value: {
+        default: 'text',
+        text: 'text',
+        arrayBuffer: 'arrayBuffer',
+        blob: 'blob',
+        document: 'document',
+        json: 'json',
+        msStream: 'ms-stream'
+    }
+});
