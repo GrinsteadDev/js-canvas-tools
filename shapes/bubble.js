7d547c5134f9b28c3b24db7db5816e7266c033af
GrinsteadDev <devin.grinstead@grinsteaddev.com>
2022-02-25 11:55:27 -0500

    Updated Bubbles.js

--- shapes/bubble.js
+++ shapes/bubble.js
@@ -82,7 +82,8 @@ function Bubble(ctx, x, y, radius, movementRate) {
             cache.ctx.fill();
             cache.ctx.closePath();
 
-            //cache.img = new Image()
+            cache.img = new Image(cache.ctx.width, cache.ctx.height);
+            cache.img.src = cache.ctx.canvas.toDataURL('image/png');
         }
     };
 
@@ -180,7 +181,7 @@ function Bubble(ctx, x, y, radius, movementRate) {
     }
 
     this.draw = function () {
-        me.ctx.drawImage(cache.ctx.canvas, 0, 0, cache.ctx.canvas.width, cache.ctx.canvas.height, me.boundingRect.x, me.boundingRect.y, me.boundingRect.width, me.boundingRect.height);
+        me.ctx.drawImage(cache.img, 0, 0, cache.img.width, cache.img.height, me.boundingRect.x, me.boundingRect.y, me.boundingRect.width, me.boundingRect.height);
     }
 
     this.update = function () {
